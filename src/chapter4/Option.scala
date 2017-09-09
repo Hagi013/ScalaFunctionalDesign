e6b0ce78f841023e90d0b3bda87ddcf8bd1e7a7f
Shuhei Hagiwara <generativepassion@gmail.com>
2017-09-10 00:55:16 +0900

    add - 4-2 ~ 4-5

--- src/chapter4/Option.scala
+++ src/chapter4/Option.scala
@@ -1,5 +1,7 @@
 package chapter4
 
+import chapter3.Cons
+
 /**
   * Created by 周平 on 2017/01/21.
   */
@@ -44,8 +46,61 @@ trait Option[+A] {
   def filter_2(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
 
-  // 4-2
-  //def variance(xs: Seq[Double]): Option[Double]
+  // 4-2 ×
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMapAns(m => mean(xs.map(x => math.pow(x - m, 2))))
+    // xs.flatMapAns((x: Double) => math.pow(x - (xs.sum) / xs.length, 2))
+  }
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _.map(f)
+  val abs0: Option[Double] => Option[Double] = lift(math.abs)
+
+  def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double = (age + numberOfSpeedingTickets).toDouble
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
+    val optAge: Option[Int] = Try(age.toInt)
+    val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
+    map2(optAge, optTickets)(insuranceRateQuote)
+  }
+
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch { case e: Exception => None}
+
+  // 4-3
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a match {
+    case Some(a) => b match {
+      case Some(b) => Some(f(a, b))
+      case None => None
+    }
+    case None => None
+  }
+
+  def map2Ans[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a flatMapAns(aa => b map(bb => f(aa,bb)))
+
+  // 4-4 ×
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMapAns(hh => sequence(t).map(hh :: _))
+  }
+
+  def parseInts(a: List[String]): Option[List[Int]] = sequence(a.map(i => Try(i.toInt)))
+
+  // 4-5 × flatMapとその中の再起処理の関係理解ができていなかった。。
+  def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => f(h).flatMapAns(hh => traverse(t)(f).map(hh :: _))
+  }
+
+  def map2For[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    for{
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -75,7 +130,6 @@ object Option {
     val n: Option[Int] = i.filter_2(_ == 5)
     val m: Option[Int] = l.filter_2(_ == 2)
     println(n, m)
-
   }
 
 }
